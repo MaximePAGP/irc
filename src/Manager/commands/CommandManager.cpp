ca8a1119ca6e0e85316b627adc6ba03b0259cb14
rgrangeon <remi.grangeoncha@gmail.com>
2025-03-28 05:58:26 +0100

    fix bug et preparation pour la suite

--- src/Manager/commands/CommandManager.cpp
+++ src/Manager/commands/CommandManager.cpp
@@ -55,6 +55,7 @@ void	CommandManager::redirectCommand(std::string command, User &user) {
 	commands["PING"] = CommandManager::handlePing;
 	commands["PRIVMSG"] = CommandManager::handlePrivmsg;
 	commands["KICK"] = CommandManager::handleUsername;
+	commands["KICK"] = CommandManager::handleKick;
 
 	if (commands.find(getFirstCommand) == commands.end()) {
 		Message::noSuchCommand(user, command);
--- src/Manager/commands/implementations/KICK.cpp
+++ src/Manager/commands/implementations/KICK.cpp
@@ -2,7 +2,7 @@
 
 void	CommandManager::handleKick(std::string param, User &user)
 {
+	User user2 = user;
 	//Server const &server = Server::getServer();
-
-
+	std::cout << "Param = " << param << std::endl;
 }
