6323fc3493b7876456dcc672058f246349a70c3c
rgrangeon <remi.grangeoncha@gmail.com>
2025-04-10 23:13:19 +0200

    ajout de securiter pour eviter un segfault

--- src/Manager/commands/implementations/KICK.cpp
+++ src/Manager/commands/implementations/KICK.cpp
@@ -2,7 +2,22 @@
 
 void	CommandManager::handleKick(std::string param, User &user)
 {
-	User user2 = user;
-	//Server const &server = Server::getServer();
+	Server const &server = Server::getServer();
+	std::string canalName = CommandManager::trimFirstParamSpace(param);
+	Canal *canal = server.getCanalByName(canalName);
+	
+	if (canal == NULL)
+	{
+		user.send_message("test\r\n");
+		std::cout << "Error" << std::endl;
+		return ;
+	}
+	if (canal->getChanOpByNickname(user.getNickName()) == NULL)
+		std::cout << "No" << std::endl;
+
+
+	//User user2 = user;
+
+	
 	std::cout << "Param = " << param << std::endl;
 }
