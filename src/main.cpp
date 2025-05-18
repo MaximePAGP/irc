b92e2ac228c6868e18a726606120679decf7d571
magrondi <mgrondinp@gmail.com>
2025-05-18 05:04:26 +0200

    clean main

--- src/main.cpp
+++ src/main.cpp
@@ -13,33 +13,20 @@ int main(int argc, char **argv)
 	if (!checkPassingArguments(argc, argv))
 		return 1;
 
-	signal(SIGINT, &handle_sigint);
 	Server &server = Server::init(std::atoi(argv[1]), argv[2]);
 	signal(SIGINT, &handle_sigint);
 	signal(SIGQUIT, &handle_sigquit);
 
-	// try
-	// {
-		User *testt = new User(
-			"salt",
-			"salu",
-			"ccpc"
-		);
-		server.addUser(*testt);
-		Canal *testCanal = new Canal("coucou");
-		testCanal->setHasProtectedTopic(false);
-		server.addCanal(*testCanal);
-		testCanal->addChanOps(*testt);
-		testCanal->addUser(*testt);
-		// std::cout << "result : " << UserManager::isUniqueUsername("salu");
+	try
+	{
 		server.running();
-	// }
-	// catch(const std::exception& e)
-	// {
-	// 	std::cerr << e.what() << std::endl;
-	// 	server.kill();
-	// 	return 1;
-	// }
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+		server.kill();
+		return 1;
+	}
 	server.kill();
 	return 0;
 }
